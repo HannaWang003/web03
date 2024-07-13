b0927727510c26f2aa2ea2154f7b7e2ad9facf1d
HannaWang003 <hannawang003@gmail.com>
2024-07-14 02:06:44 +0800

    init-practice

--- api/checkout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "db.php";
-$id = $Order->max('id', ['date' => $_POST['date']]) + 1;
-echo $_POST['no'] = date("Ymd") . sprintf("%04d", $id);
-sort($_POST['seats']);
-$_POST['seats'] = serialize($_POST['seats']);
-$Order->save($_POST);
