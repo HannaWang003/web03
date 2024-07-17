738ef8c256c0bfda1f127284440805b923b90eb5
HannaWang003 <hannawang003@gmail.com>
2024-07-18 00:55:27 +0800

    init p2

--- api/del.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once "db.php";
-$table = $_GET['table'];
-$DB = ${ucfirst($table)};
-$DB->del($_GET['id']);
--- api/sh.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once "db.php";
-$row = $Movie->find($_GET['id']);
-$row['sh'] = ($row['sh'] == 0) ? 1 : 0;
-$Movie->save($row);
