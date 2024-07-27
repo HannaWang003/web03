7559f4d3898c45455ad3e4c282105728e76a3a57
HannaWang003 <hannawang003@gmail.com>
2024-07-28 01:47:23 +0800

    init p4

--- api/order.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "db.php";
-$no = $Order->max('no', ['orderdate' => date("Y-m-d")]);
-echo $_POST['no'] = ($no == 0) ? date("Ymd") . "0001" : (int)$no + 1;
-$_POST['orderdate'] = date("Y-m-d");
-$_POST['seat'] = serialize($_POST['seat']);
-$Order->save($_POST);
--- api/sh.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once "db.php";
-$row = $Movie->find($_POST['id']);
-$row['sh'] = ($row['sh'] == 1) ? 0 : 1;
-$Movie->save($row);
