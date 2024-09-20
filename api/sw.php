5074c540bf53f389fac207ac65bde1ff1a20ad4c
HannaWang003 <hannawang003@gmail.com>
2024-09-21 01:13:47 +0800

    p6 init

--- api/sw.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "db.php";
-$now = $DB->find($_GET['id']);
-$sw = $DB->find($_GET['sw']);
-$tmp = $now['rank'];
-$now['rank'] = $sw['rank'];
-$sw['rank'] = $tmp;
-$DB->save($now);
-$DB->save($sw);
