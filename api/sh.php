e495d8406e91dd8bbd15d8ec3e0c0c2109ae46a1
HannaWang003 <hannawang003@gmail.com>
2024-09-24 00:58:40 +0800

    p7 init

--- api/sh.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once "db.php";
-$_POST['sh'] = ($_POST['sh'] == 1) ? "0" : "1";
-$DB->save($_POST);
--- api/sw.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "db.php";
-$now = $DB->find($_POST['id']);
-$sw = $DB->find($_POST['sw']);
-$tmp = $now['rank'];
-$now['rank'] = $sw['rank'];
-$sw['rank'] = $tmp;
-$DB->save($now);
-$DB->save($sw);
