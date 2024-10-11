187f4f6dde0f0453e8615a46ebd374bb3ee567b8
HannaWang003 <hannawang003@gmail.com>
2024-10-11 23:02:13 +0800

    init p9

--- api/checkout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "db.php";
-$_POST['seats'] = serialize($_POST['seats']);
-$today = date("Ymd");
-$no = $DB->max('no', "where `no` like '$today%'");
-echo $_POST['no'] = ($no == 0) ? $today . "0001" : $no + 1;
-$DB->save($_POST);
--- api/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-for ($i = 1; $i <= $pages; $i++) {
-    $style = ($now == $i) ? "font-size:22px" : "";
-?>
-    <a href="?p=<?= $i ?>" style="color:#fff;<?= $style ?>"><?= $i ?></a>
-<?php
-}
-?>
