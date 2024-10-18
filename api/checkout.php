b344b0ae88a12b1a3fe6d6c0b77b9c55832a9de7
HannaWang003 <hannawang003@gmail.com>
2024-10-18 23:40:14 +0800

    init p10

--- api/checkout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "db.php";
-$today = date("Ymd");
-$max = ($DB->max("no", "where `no` like '$today%'")) ?? $today . "0000";
-echo $_POST['no'] = $max + 1;
-$_POST['seat'] = serialize($_POST['seat']);
-$DB->save($_POST);
